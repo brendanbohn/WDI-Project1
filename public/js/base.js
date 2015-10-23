67cff80fd12f3b2ccf64522557514376dd32698f
brendanbohn <brendanbohn@comcast.net>
2015-10-23 11:01:48 -0700

    profile page log a trip modal form

--- public/js/base.js
+++ public/js/base.js
@@ -1,12 +1,10 @@
 console.log('Sanity check, client-side JS is working.');
 
-var map;
-
 $(document).ready(function(){
 
-	map = new google.maps.Map(document.getElementById('map'), {
-	  center: {lat: 37.78, lng: -122.44},
-	  zoom: 3
+	$('#trip-form').submit(function(e){
+		e.preventDefault();
+		console.log('Prevented default on the trip submit form');
 	});
 
 });
